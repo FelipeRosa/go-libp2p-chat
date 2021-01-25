0c865d73e6dcde75911ee49149e1858cfa2e7fd0
Felipe Rosa <felipe.sgrosa@gmail.com>
2021-01-25 19:27:46 -0300

    app: fix 'connection dropped' error when exiting

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -19,13 +19,16 @@ import { ChatMessage, LocalNodeInfo } from "../common/ipc"
 class State {
     goNode: ChildProcessWithoutNullStreams | null
     apiClient: ApiClient | null
+    newMessagesStream: grpc.ClientReadableStream<ApiChatMessage> | null
 
     constructor() {
         this.goNode = null
         this.apiClient = null
+        this.newMessagesStream = null
     }
 
     close() {
+        this.newMessagesStream?.cancel()
         this.apiClient?.close()
         this.goNode?.kill()
     }
@@ -81,154 +84,186 @@ app.whenReady().then(() => {
     ])
     Menu.setApplicationMenu(menu)
 
-    ipcMain.on("chat.connect", (_e, nickname: string, bootstrapAddrs: string) => {
-        state.close()
+    ipcMain.on(
+        "chat.connect",
+        (_e, nickname: string, bootstrapAddrs: string) => {
+            state.close()
 
-        Promise.all([
-            getPort(),
-            getPort(),
-        ]).then(([nodePort, apiPort]) => {
-            const goNodeArgs: string[] = [
-                "-port",
-                nodePort.toString(),
-                "-api-port",
-                apiPort.toString(),
-            ]
-            if (bootstrapAddrs.length > 0) {
-                goNodeArgs.push("-bootstrap-addrs")
-                goNodeArgs.push(bootstrapAddrs.trim())
-            }
-
-            console.log(
-                `starting chat-gonode with args: ${JSON.stringify(goNodeArgs)}`,
-            )
-
-            state.goNode = child_process.spawn(
-                path.join(app.getAppPath(), "chat-gonode"),
-                goNodeArgs,
-                {
-                    detached: false,
-                },
-            )
-            state.goNode.stderr.on("data", (d: Buffer) =>
-                console.log("chat-gonode:", d.toString()),
-            )
-            state.goNode.on("close", () =>
-                console.log("chat-gonode closed", state.goNode?.exitCode),
-            )
-
-            const tryConnect = async () => {
-                let nodeId = ""
-
-                while (true) {
-                    console.log("polling node...")
-                    try {
-                        state.apiClient = new ApiClient(
-                            `localhost:${apiPort}`,
-                            grpc.credentials.createInsecure(),
-                        )
-
-                        nodeId = await new Promise<string>(
-                            (resolve, reject) => {
-                                if (state.apiClient === null) {
-                                    return reject()
-                                }
-
-                                state.apiClient.getNodeID(
-                                    new GetNodeIDRequest(),
-                                    (err, res) => {
-                                        if (err !== null) {
-                                            return reject(err)
-                                        }
-                                        resolve(res?.getId() || "")
-                                    },
-                                )
-                            },
-                        )
-
-                        break
-                    } catch (e) {
-                        await new Promise((resolve) => setTimeout(resolve, 200))
-                    }
+            Promise.all([getPort(), getPort()]).then(([nodePort, apiPort]) => {
+                const goNodeArgs: string[] = [
+                    "-port",
+                    nodePort.toString(),
+                    "-api-port",
+                    apiPort.toString(),
+                ]
+                if (bootstrapAddrs.length > 0) {
+                    goNodeArgs.push("-bootstrap-addrs")
+                    goNodeArgs.push(bootstrapAddrs.trim())
                 }
 
-                // we are connected
-                // subscribe to new messages
-                const newMessagesStream = state.apiClient.subscribeToNewMessages(
-                    new SubscribeToNewMessagesRequest(),
+                console.log(
+                    `starting chat-gonode with args: ${JSON.stringify(
+                        goNodeArgs,
+                    )}`,
                 )
-                newMessagesStream.on("data", (msg: ApiChatMessage) => {
-                    const senderId = msg.getSenderId()
 
-                    // need to get the sender nickname
-                    const getNicknameReq = new GetNicknameRequest()
-                    getNicknameReq.setPeerId(senderId)
-                    state.apiClient?.getNickname(getNicknameReq, (err, res) => {
-                        window.webContents.send(`chat.new-message`, {
-                            sender: {
-                                id: senderId,
-                                nickname: res?.getNickname() || "Unnamed",
-                            },
-                            timestamp: msg.getTimestamp(),
-                            value: msg.getValue(),
-                        } as ChatMessage)
-                    })
-                })
-                newMessagesStream.on("end", () => console.log("stream ended"))
+                state.goNode = child_process.spawn(
+                    path.join(app.getAppPath(), "chat-gonode"),
+                    goNodeArgs,
+                    {
+                        detached: false,
+                    },
+                )
+                state.goNode.stderr.on("data", (d: Buffer) =>
+                    console.log("chat-gonode:", d.toString()),
+                )
+                state.goNode.on("close", () =>
+                    console.log("chat-gonode closed", state.goNode?.exitCode),
+                )
 
-                const setNicknameReq = new SetNicknameRequest()
-                setNicknameReq.setNickname(nickname)
-                const setNickname = new Promise<void>((resolve, reject) => {
-                    if (state.apiClient === null) {
-                        return reject()
+                const tryConnect = async () => {
+                    let nodeId = ""
+
+                    while (true) {
+                        console.log("polling node...")
+                        try {
+                            state.apiClient = new ApiClient(
+                                `localhost:${apiPort}`,
+                                grpc.credentials.createInsecure(),
+                            )
+
+                            nodeId = await new Promise<string>(
+                                (resolve, reject) => {
+                                    if (state.apiClient === null) {
+                                        return reject()
+                                    }
+
+                                    state.apiClient.getNodeID(
+                                        new GetNodeIDRequest(),
+                                        (err, res) => {
+                                            if (err !== null) {
+                                                return reject(err)
+                                            }
+                                            resolve(res?.getId() || "")
+                                        },
+                                    )
+                                },
+                            )
+
+                            break
+                        } catch (e) {
+                            await new Promise((resolve) =>
+                                setTimeout(resolve, 200),
+                            )
+                        }
                     }
 
-                    state.apiClient.setNickname(setNicknameReq, (err, res) => {
-                        if (err !== null) {
-                            return reject(err)
-                        }
-                        resolve()
-                    })
-                })
-                await setNickname
+                    // we are connected
+                    // subscribe to new messages
+                    state.newMessagesStream = state.apiClient.subscribeToNewMessages(
+                        new SubscribeToNewMessagesRequest(),
+                    )
+                    state.newMessagesStream.on(
+                        "data",
+                        (msg: ApiChatMessage) => {
+                            const senderId = msg.getSenderId()
 
-                const getCurrentRoomName = new Promise<string>(
-                    (resolve, reject) => {
+                            // need to get the sender nickname
+                            const getNicknameReq = new GetNicknameRequest()
+                            getNicknameReq.setPeerId(senderId)
+                            state.apiClient?.getNickname(
+                                getNicknameReq,
+                                (err, res) => {
+                                    const chatMessage: ChatMessage = {
+                                        sender: {
+                                            id: senderId,
+                                            nickname:
+                                                res?.getNickname() || "Unnamed",
+                                        },
+                                        timestamp: msg.getTimestamp(),
+                                        value: msg.getValue(),
+                                    }
+
+                                    window.webContents.send(
+                                        `chat.new-message`,
+                                        chatMessage,
+                                    )
+                                },
+                            )
+                        },
+                    )
+                    state.newMessagesStream.on(
+                        "error",
+                        (err: grpc.ServiceError) => {
+                            if (err.code === 1) {
+                                return
+                            }
+                            throw err
+                        },
+                    )
+                    state.newMessagesStream.on("end", () =>
+                        console.log("stream ended"),
+                    )
+
+                    const setNicknameReq = new SetNicknameRequest()
+                    setNicknameReq.setNickname(nickname)
+                    const setNickname = new Promise<void>((resolve, reject) => {
                         if (state.apiClient === null) {
                             return reject()
                         }
 
-                        state.apiClient.getCurrentRoomName(
-                            new GetCurrentRoomNameRequest(),
+                        state.apiClient.setNickname(
+                            setNicknameReq,
                             (err, res) => {
                                 if (err !== null) {
-                                    return reject()
+                                    return reject(err)
                                 }
-                                resolve(res?.getRoomName() || "No room name")
+                                resolve()
                             },
                         )
-                    },
-                )
-                const currentRoomName = await getCurrentRoomName
+                    })
+                    await setNickname
 
-                return [nodeId, currentRoomName]
-            }
+                    const getCurrentRoomName = new Promise<string>(
+                        (resolve, reject) => {
+                            if (state.apiClient === null) {
+                                return reject()
+                            }
 
-            tryConnect()
-                .then(([nodeId, roomName]) => {
-                    console.log(`connected to local node ID ${nodeId}`)
-                    window.webContents.send("chat.connected", {
-                        address: `/ip4/127.0.0.1/tcp/${nodePort}/p2p/${nodeId}`,
-                        id: nodeId,
-                        nickname: nickname,
-                        currentRoomName: roomName,
-                    } as LocalNodeInfo)
-                })
-                .catch((err) => {
-                    dialog.showErrorBox("Error", err.toString())
-                })
-        })
-    })
+                            state.apiClient.getCurrentRoomName(
+                                new GetCurrentRoomNameRequest(),
+                                (err, res) => {
+                                    if (err !== null) {
+                                        return reject()
+                                    }
+                                    resolve(
+                                        res?.getRoomName() || "No room name",
+                                    )
+                                },
+                            )
+                        },
+                    )
+                    const currentRoomName = await getCurrentRoomName
+
+                    return [nodeId, currentRoomName]
+                }
+
+                tryConnect()
+                    .then(([nodeId, roomName]) => {
+                        console.log(`connected to local node ID ${nodeId}`)
+                        window.webContents.send("chat.connected", {
+                            address: `/ip4/127.0.0.1/tcp/${nodePort}/p2p/${nodeId}`,
+                            id: nodeId,
+                            nickname: nickname,
+                            currentRoomName: roomName,
+                        } as LocalNodeInfo)
+                    })
+                    .catch((err) => {
+                        dialog.showErrorBox("Error", err.toString())
+                    })
+            })
+        },
+    )
 
     ipcMain.on("chat.send", (_e, msg: string) => {
         const request = new SendMessageRequest()
